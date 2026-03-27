57f3c55cf5f59c8e35695b83e3f9d5c25e110a93
phongntn <phongntn1@gmail.com>
2026-03-27 08:51:34 +0700

    Exercise completed

--- lab01/src/FirstDialog.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello World!");
-        System.exit(0);
-    }
-}
