1c0281d6c8f18bc93a2f598065015e4103795152
phongntn <phongntn1@gmail.com>
2026-04-14 14:49:34 +0700

    Convert lab folders from submodules to regular directories

---
+++ lab01/src/ex_2_2_2/FirstDialog.java
@@ -0,0 +1,10 @@
+package ex_2_2_2;
+
+import javax.swing.*;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World!");
+        System.exit(0);
+    }
+}
---
+++ lab01/src/ex_2_2_5/CalcTwoNumbers.java
@@ -0,0 +1,59 @@
+package ex_2_2_5;
+
+import javax.swing.*;
+
+public class CalcTwoNumbers {
+    public  static void main(String args[]) {
+        String strNum1, strNum2;
+        String strSum = "Sum of two numbers: ";
+        String strDiff = "Difference of two numbers: ";
+        String strProduct =  "Product of two numbers: ";
+        String strQuotient = "Quotient of two numbers: ";
+
+        strNum1 = JOptionPane.showInputDialog(null,
+                "Please input the first number: ",
+                "Input the first number",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        strNum2 = JOptionPane.showInputDialog(null,
+                "Please input the second number: ",
+                "Input the second number",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        int num1 = Integer.parseInt(strNum1);
+        int num2 = Integer.parseInt(strNum2);
+
+        double num1q = Double.parseDouble(strNum1);
+        double num2q = Double.parseDouble(strNum2);
+
+        strSum += String.valueOf(num1 + num2);
+        strDiff += String.valueOf(num1 - num2);
+        strProduct += String.valueOf(num1 * num2);
+
+
+        JOptionPane.showMessageDialog(null, strSum,
+                "Sum of two numbers",
+                JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, strDiff,
+                "Difference of two numbers",
+                JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, strProduct,
+                "Product of two numbers",
+                JOptionPane.INFORMATION_MESSAGE);
+
+        if (num2q != 0) {
+            strQuotient += String.valueOf(num1q / num2q);
+            JOptionPane.showMessageDialog(null, strQuotient,
+                    "Quotient of two numbers",
+                    JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        else {
+            JOptionPane.showMessageDialog(null,
+                    "No solution", "Quotient of two numbers",
+                    JOptionPane.INFORMATION_MESSAGE);
+        }
+
+        System.exit(0);
+    }
+}
