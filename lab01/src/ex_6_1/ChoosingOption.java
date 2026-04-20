db5020bf21fba6ddc394b10cb3180ce40cbf6d39
phongntn <phongntn1@gmail.com>
2026-04-20 23:58:17 +0700

    Lab 01 lab 02 finished

--- lab01/src/ex_6_1/ChoosingOption.java
+++ lab01/src/ex_6_1/ChoosingOption.java
@@ -4,9 +4,12 @@ import javax.swing.*;
 
 public class ChoosingOption {
     public static void main(String args[]) {
-        int option = JOptionPane.showConfirmDialog(null,
+
+        String[] options = {"I do", "I don't"};
+
+        int option = JOptionPane.showOptionDialog(null,
                 "Do you want to change to the first class ticket?",
-                null, JOptionPane.YES_NO_OPTION);
+                null, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
         JOptionPane.showMessageDialog(null, "You've chosen: "
         + (option == JOptionPane.YES_OPTION ? "Yes" : "No"));
--- lab03/src/TestPassingParameter.java
+++ lab03/src/TestPassingParameter.java
@@ -5,20 +5,18 @@ public class TestPassingParameter {
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
         DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 
-        DigitalVideoDisc[] dvds = { jungleDVD, cinderellaDVD };
-
-        swap(dvds);
-        System.out.println("jungle dvd title: " + dvds[0].getTitle());
-        System.out.println("cinderella dvd title: " + dvds[1].getTitle());
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
 
         changeTitle(jungleDVD, cinderellaDVD.getTitle());
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
     }
 
-    public static void swap(DigitalVideoDisc[] dvds) {
-        DigitalVideoDisc temp = dvds[0];
-        dvds[0] = dvds[1];
-        dvds[1] = temp;
+    public static void swap(Object o1, Object o2) {
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
     }
 
     public static void changeTitle(DigitalVideoDisc dvd, String title) {
