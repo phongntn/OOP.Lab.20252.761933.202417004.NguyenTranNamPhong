2d5378dd79e8295b5df61d0df2035d034bd4f199
phongntn <phongntn1@gmail.com>
2026-04-24 22:31:22 +0700

    Update passing parameter

---
+++ lab03/src/TestPassingParameter.java
@@ -0,0 +1,31 @@
+public class TestPassingParameter {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        DigitalVideoDisc[] pair = {jungleDVD, cinderellaDVD};
+        swap(pair);
+
+        jungleDVD = pair[0];
+        cinderellaDVD = pair[1];
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(DigitalVideoDisc[] arr) {
+        DigitalVideoDisc tmp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = tmp;
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
+}
